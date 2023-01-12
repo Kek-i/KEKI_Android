4f230184ce5edf97534a2501dd51e4da33552a51
OkHui <100889562+1000oki@users.noreply.github.com>
2023-01-12 23:33:46 +0900

    가게 메인화면, 가게 메인화면-1 구현

---
+++ app/src/main/java/com/umc/keki/src/store/StoreMainActivity.kt
@@ -0,0 +1,45 @@
+package com.umc.keki.src.store
+
+
+import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.umc.keki.R
+import com.umc.keki.config.BaseActivity
+import com.umc.keki.databinding.ActivityStoreMainBinding
+import com.umc.keki.util.viewpager.storemain.StoreMainVPAdapter
+
+class StoreMainActivity : BaseActivity<ActivityStoreMainBinding>(ActivityStoreMainBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val storeMainVPAdapter = StoreMainVPAdapter(this)
+        binding.vpStore.adapter = storeMainVPAdapter
+
+        val tabIconArray = arrayOf(R.drawable.ic_store_main_grid, R.drawable.ic_store_main_cake)
+
+        TabLayoutMediator(binding.tabStore, binding.vpStore){tab, position ->
+            tab.setIcon(tabIconArray[position])
+        }.attach()
+
+        binding.tabStore.getTabAt(0)?.icon?.setTint(ResourcesCompat.getColor(getResources(),R.color.tab_select,null))
+
+        binding.tabStore.addOnTabSelectedListener(object:TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.icon?.setTint(ResourcesCompat.getColor(getResources(),R.color.tab_select,null))
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.icon?.setTint(ResourcesCompat.getColor(getResources(),R.color.tab_unselect,null))
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
+
+
+
