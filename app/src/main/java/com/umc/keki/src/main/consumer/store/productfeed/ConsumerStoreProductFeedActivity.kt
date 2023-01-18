9ef5f86497e519ec5dd51e5704e9728c29ac8e73
ddubii <wow_boo@sookmyung.ac.kr>
2023-01-18 15:55:45 +0900

    feat: store product detail feed UI

---
+++ app/src/main/java/com/umc/keki/src/main/consumer/store/productfeed/ConsumerStoreProductFeedActivity.kt
@@ -0,0 +1,52 @@
+package com.umc.keki.src.main.consumer.store.productfeed
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.widget.TextView
+import com.umc.keki.R
+import com.umc.keki.config.BaseActivity
+import com.umc.keki.databinding.ActivityStoreProductFeedBinding
+import com.umc.keki.src.main.consumer.store.DetailImageAdapter
+
+class ConsumerStoreProductFeedActivity : BaseActivity<ActivityStoreProductFeedBinding>(ActivityStoreProductFeedBinding::inflate) {
+
+    val storeProductFeedDatas = mutableListOf<StoreProductFeedData>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navigateToStoreMain()
+
+        storeProductFeedDatas.apply { add(StoreProductFeedData("케키 나라", "달달한 딸케", "25000원", "설명명며염ㅇ")) }
+        getData()
+    }
+
+    private fun getData(){
+        val storeId: TextView = binding.tvProductFeedId
+        val cakeName: TextView = binding.tvProductFeedCakeName
+        val cakePrice: TextView = binding.tvProductFeedCakePrice
+        val cakeDetail: TextView = binding.tvProductFeedCakeDetail
+        storeId.text = storeProductFeedDatas[0].storeName
+        cakeName.text = storeProductFeedDatas[0].cakeName
+        cakePrice.text = storeProductFeedDatas[0].cakePrice
+        cakeDetail.text = storeProductFeedDatas[0].cakeDetail
+
+        // 나중에 데이터 구조 보고 변경 - ConsumerStoreFeedActivity
+        var img = arrayOfNulls<Drawable>(4)
+
+        img[0] = this@ConsumerStoreProductFeedActivity?.getDrawable(R.drawable.ex_cake)
+        img[1] = this@ConsumerStoreProductFeedActivity?.getDrawable(R.drawable.img_cake)
+        img[2] = this@ConsumerStoreProductFeedActivity?.getDrawable(R.drawable.ex_cake)
+        img[3] = this@ConsumerStoreProductFeedActivity?.getDrawable(R.drawable.img_cake)
+
+        val pagerAdapter = DetailImageAdapter(this@ConsumerStoreProductFeedActivity!!, img)
+        binding.vpProductFeedImg.adapter = pagerAdapter
+        binding.wormDotsIndicator.setViewPager2(binding.vpProductFeedImg)
+    }
+
+    private fun navigateToStoreMain(){
+        binding.ivProductFeedLeftChevron.setOnClickListener {
+        finish()
+        }
+    }
+}
+
