defabf9e5ace3f430a33f10480261679edd0f762
ddubii <wow_boo@sookmyung.ac.kr>
2023-01-19 12:13:32 +0900

    Update ConsumerStoreProductDetailFeedActivity.kt

--- app/src/main/java/com/umc/keki/src/main/consumer/store/productfeed/ConsumerStoreProductDetailFeedActivity.kt
+++ app/src/main/java/com/umc/keki/src/main/consumer/store/productfeed/ConsumerStoreProductDetailFeedActivity.kt
@@ -9,24 +9,23 @@ import com.umc.keki.databinding.ActivityConsumerStoreProductDetailFeedBinding
 
 class ConsumerStoreProductDetailFeedActivity : BaseActivity<ActivityConsumerStoreProductDetailFeedBinding>(ActivityConsumerStoreProductDetailFeedBinding::inflate) {
 
-    val storeProductFeedDatas = mutableListOf<StoreProductFeedData>()
+    val storeProductFeedData = mutableListOf<StoreProductFeedData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         navigateToStoreMain()
 
-        storeProductFeedDatas.apply { add(StoreProductFeedData("케키 나라", "딸기 티라미수", "25000원", "설명설명\n레터링 추가 +2000\n디자인 추가 +5000")) }
+        storeProductFeedData.apply { add(StoreProductFeedData("케키 나라", "딸기 티라미수", "25000원", "설명설명\n레터링 추가 +2000\n디자인 추가 +5000")) }
         getData()
     }
 
     private fun getData(){
-        binding.tvProductFeedId.text = storeProductFeedDatas[0].storeName
-        binding.tvProductFeedCakeName.text = storeProductFeedDatas[0].cakeName
-        binding.tvProductFeedCakePrice.text = storeProductFeedDatas[0].cakePrice
-        binding.tvProductFeedCakeDetail.text = storeProductFeedDatas[0].cakeDetail
+        binding.tvProductFeedId.text = storeProductFeedData[0].storeName
+        binding.tvProductFeedCakeName.text = storeProductFeedData[0].cakeName
+        binding.tvProductFeedCakePrice.text = storeProductFeedData[0].cakePrice
+        binding.tvProductFeedCakeDetail.text = storeProductFeedData[0].cakeDetail
        
 
-        // 나중에 데이터 구조 보고 변경 - ConsumerStoreFeedActivity
         var img = arrayOfNulls<Drawable>(4)
 
         img[0] = this@ConsumerStoreProductDetailFeedActivity?.getDrawable(R.drawable.ex_cake)
