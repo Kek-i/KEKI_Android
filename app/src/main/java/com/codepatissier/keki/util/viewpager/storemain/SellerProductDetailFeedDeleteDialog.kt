2bad78e47750c4da8eb318327bcafc375849a0c3
ddubii <wow_boo@sookmyung.ac.kr>
2023-01-22 04:32:05 +0900

    feat: 삭제 다이얼로그 생성

---
+++ app/src/main/java/com/codepatissier/keki/util/viewpager/storemain/SellerProductDetailFeedDeleteDialog.kt
@@ -0,0 +1,39 @@
+package com.codepatissier.keki.util.viewpager.storemain
+
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Window
+import com.codepatissier.keki.databinding.DialogSellerProductDetailFeedDeleteBinding
+import com.codepatissier.keki.src.main.login.IntroActivity
+
+
+class SellerProductDetailFeedDeleteDialog(context: Context): Dialog(context) {
+    private lateinit var  binding : DialogSellerProductDetailFeedDeleteBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        binding = DialogSellerProductDetailFeedDeleteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window!!.setBackgroundDrawable(ColorDrawable())
+        window!!.setDimAmount(0.2f)
+
+        checkBtn()
+    }
+
+    override fun show(){
+        if(!this.isShowing) super.show()
+    }
+
+    override fun onStop() {
+        dismiss()
+    }
+
+    private fun checkBtn(){
+        binding.btnDelete.setOnClickListener{
+            this.dismiss()
+        }
+    }
+}
