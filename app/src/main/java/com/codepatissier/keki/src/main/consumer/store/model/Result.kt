dc5a64b82e3043090f9598496493ae574ffc55df
OkHui <100889562+1000oki@users.noreply.github.com>
2023-01-26 10:35:13 +0900

    feat : 서버 주문 링크 추가

--- app/src/main/java/com/codepatissier/keki/src/main/consumer/store/model/Result.kt
+++ app/src/main/java/com/codepatissier/keki/src/main/consumer/store/model/Result.kt
@@ -3,5 +3,6 @@ package com.codepatissier.keki.src.main.consumer.store.model
 data class Result (
     val nickname: String,
     val storeImgUrl: String,
-    val introduction: String
+    val introduction: String,
+    val  orderUrl:String
 )
