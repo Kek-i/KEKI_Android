87eb5d4fac816213fac26efd13d097b45407d168
ddubii <wow_boo@sookmyung.ac.kr>
2023-01-31 16:18:04 +0900

    feat: 로그아웃, 회원탈퇴 서버 연결

--- app/src/main/java/com/codepatissier/keki/util/dialog/WithdrawalDialog.kt
+++ app/src/main/java/com/codepatissier/keki/util/dialog/WithdrawalDialog.kt
@@ -2,12 +2,19 @@ package com.codepatissier.keki.util.dialog
 
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Window
+import android.widget.Toast
+import com.codepatissier.keki.config.ApplicationClass
+import com.codepatissier.keki.config.BaseResponse
 import com.codepatissier.keki.databinding.DialogWithdrawalBinding
+import com.codepatissier.keki.src.MainActivity
+import com.codepatissier.keki.src.main.auth.SignoutService
+import com.codepatissier.keki.src.main.auth.SignoutView
 
-class WithdrawalDialog(context: Context): Dialog(context) {
+class WithdrawalDialog(context: Context): Dialog(context), SignoutView {
     private lateinit var binding: DialogWithdrawalBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +38,20 @@ class WithdrawalDialog(context: Context): Dialog(context) {
 
     private fun clickWithdrawalBtn() {
         binding.btnWithdrawal.setOnClickListener {
-
+            SignoutService(this).tryPatchSignout()
         }
+    }
+
+    override fun onPatchSignoutSuccess(response: BaseResponse) {
+        this.dismiss()
+        //확인 버튼 눌렀을 때 종료 flag
+        ApplicationClass.userInfo.remove("Authorization");
+        ApplicationClass.userInfo.commit();
+        val intent = Intent(context, MainActivity::class.java)
+        context.startActivity(intent)
+        Toast.makeText(context, "회원탈퇴 완료", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPatchSignoutFailure(message: String) {
     }
 }
