cf27bb05c68206dfa1c41f7a373c360cfb5bc827
YuJungHyun <0703olivia@naver.com>
2023-03-30 17:24:01 +0900

    feat: 확인 버튼 클릭 후 StoreMainActivity로 이동

--- app/src/main/java/com/codepatissier/keki/src/main/consumer/store/order/finish/ConsumerOrderCompleteActivity.kt
+++ app/src/main/java/com/codepatissier/keki/src/main/consumer/store/order/finish/ConsumerOrderCompleteActivity.kt
@@ -1,14 +1,25 @@
 package com.codepatissier.keki.src.main.consumer.store.order.finish
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codepatissier.keki.config.BaseActivity
 import com.codepatissier.keki.databinding.ActivityConsumerOrderCompleteBinding
+import com.codepatissier.keki.src.main.consumer.store.ConsumerStoreMainActivity
 
 class ConsumerOrderCompleteActivity : BaseActivity<ActivityConsumerOrderCompleteBinding>(ActivityConsumerOrderCompleteBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        confirmOrder()
+    }
+
+    private fun confirmOrder(){
+        binding.tvOrderCompleteOk.setOnClickListener {
+            var intent = Intent(this, ConsumerStoreMainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
     }
 }
