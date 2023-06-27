8d715f419b8a497a9d55791581e7f10969d14458
ddubii <wow_boo@sookmyung.ac.kr>
2023-06-27 20:54:25 +0900

    feat: mypage 주문차트 api연결

--- app/src/main/java/com/codepatissier/keki/src/main/consumer/mypage/model/MyPageResult.kt
+++ app/src/main/java/com/codepatissier/keki/src/main/consumer/mypage/model/MyPageResult.kt
@@ -2,5 +2,8 @@ package com.codepatissier.keki.src.main.consumer.mypage.model
 
 class MyPageResult (
     val nickname : String,
-    val profileImg : String
+    val profileImg : String,
+    val orderWaiting: Int,
+    val progressing: Int,
+    val pickupWaiting: Int
 )
